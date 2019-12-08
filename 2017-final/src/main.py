00841dcccd6cfdd430ab03530c5280dad9a38590
darkloner99 <antoine.blaud@gmail.com>
2019-12-09 00:33:37 +0100

    added splice per zone

--- 2017-final/src/main.py
+++ 2017-final/src/main.py
@@ -6,12 +6,11 @@ import re
 import argparse
 
 parser = argparse.ArgumentParser(description='Hashcode 2017-----final')
-parser.add_argument("-v",action='store', dest='verbose',help='Enable verbose')
+parser.add_argument("-v",action='store', dest='verbose',help='enable verbose')
 args = parser.parse_args()
  
 CHARLESTON_IN= "/home/darkloner99/code/Hashcode/Hashcode/2017-final/final_round_2017.in/charleston_road.in"
 TEST_IN = "/home/darkloner99/code/Hashcode/Hashcode/2017-final/final_round_2017.in/test.In"
-
 CHARLESTON_ROUTER_OUT ="/home/darkloner99/code/Hashcode/Hashcode/2017-final/out/charleston_router.out"
 CHARLESTON_TARGET_OUT ="/home/darkloner99/code/Hashcode/Hashcode/2017-final/out/charleston_target.out"
 
@@ -24,25 +23,40 @@ backboneX, backboneY = map(int,[int(x) for x in re.sub("\n","",f.readline()).spl
 # Trouver compromis entre budget, nombre de cellule qui capte pour avoir meilleur score possible
 router_budget = 20000
 bone_budget = 20
-
 mapping=[]
 for i in range(rowsN):
-    line = re.sub("\n","",f.readline())
-    mapping.append([])
+    line = re.sub("\n","",f.readline());mapping.append([])
     for j in range(columsN):
-        if(line[j]=="-"):
-            temp = 0
-        if(line[j]=="."):
-            temp = 1
-        if(line[j]=="#"):
-            temp = 2
+        if(line[j]=="-"): temp = 0
+        if(line[j]=="."): temp = 1
+        if(line[j]=="#"): temp = 2
         mapping[i].append(temp)
 f.close()
 
 def reset_output_file(file):
-    f = open(file,"w")
-    f.close()
+    f = open(file,"w");f.close()
 
+def write_output(fileRouter,mapping):
+    def convert(i:int):
+        '''
+        Convertie de entier a string
+        '''
+        if(i==0): return "-"
+        if(i==1): return "."
+        if(i==2): return "#"
+        if(i==3): return "r"
+
+    def write_router_output(file,mapping):
+        '''
+        Ecrit la solution du placement des routeurs
+        '''
+        rowsN = len(mapping); columsN = len(mapping[0]);f = open(file,"w")
+        for i in range(rowsN):
+            for j in range(columsN):
+                f.write(convert(mapping[i][j]))
+            f.write("\n")
+        f.close()
+    write_router_output(fileRouter,mapping)
 
 
 def process_router(mapping,router_budget,maxSeconds=None):
@@ -54,7 +68,6 @@ def process_router(mapping,router_budget,maxSeconds=None):
     columsN = len(mapping[0])
     mapping_router=[]
     mapping_router = [(i,j) for i in range(rowsN) for j in range(columsN)]
-
     prob = pulp.LpProblem("2017-final", pulp.LpMaximize)
     targetcell = [(x,y) for x in range(rowsN) for y in range(columsN)]
     targetcell_receive=  [(x,y,z) for x in range(rowsN) for y in range(columsN) for z in range((2*radius+1)*(2*radius+1))]
@@ -63,14 +76,13 @@ def process_router(mapping,router_budget,maxSeconds=None):
     targetcell_receive = pulp.LpVariable.dicts("TargetCellReceive", targetcell_receive,lowBound=0, upBound=1, cat=pulp.LpInteger)
     # 0 = rien, 1 = router
     mapping_router = pulp.LpVariable.dicts("MappingRouter", mapping_router,lowBound=0, upBound=1, cat=pulp.LpInteger)
-
     # Variable de décision pour le IF
     omega = [(i,j) for i in range(0,rowsN) for j in range(columsN)]
     omega = pulp.LpVariable.dicts("Omega", omega,lowBound=0, upBound=1, cat=pulp.LpInteger)
 
+
     # Maximiser le score (cellule qui capte)
     prob+=pulp.lpSum([targetcell[(x,y)] for x in range(rowsN) for y in range(columsN)])
-
     #   CONTRAINTES
     # Limiter le budget
     prob+=pulp.lpSum([mapping_router[(x,y)] for x in range(rowsN) for y in range(columsN)]) <= max_router
@@ -80,57 +92,20 @@ def process_router(mapping,router_budget,maxSeconds=None):
     prob+=pulp.lpSum([targetcell[(x,y)] for x in range(rowsN) for y in range(columsN) if mapping[x][y]==0]) == 0
     prob+=pulp.lpSum([targetcell[(x,y)] for x in range(rowsN) for y in range(columsN) if mapping[x][y]==2]) == 0
 
+    '''Retourne 1 si un mur bloque le passage des ondes entre la targetcell et le répeteur'''
     def check_wall(ti,tj,ri,rj):
-        '''
-        Retourne 1 si un mur bloque le passage des ondes entre la targetcell et le répeteur
-        '''
         for j in range(min(rj,tj),max(rj,tj)+1):
-            if(mapping[ri][j]==2):
-                return 1
-            if(mapping[ti][j]==2):
-                return 1
+            if(mapping[ri][j]==2): return 1
+            if(mapping[ti][j]==2): return 1
         for i in range(min(ti,ri),max(ti,ri)+1):
-            if(mapping[i][rj]==2):
-                return 1
-            if(mapping[i][tj]==2):
-                return 1
+            if(mapping[i][rj]==2): return 1
+            if(mapping[i][tj]==2): return 1
         return 0    
-    def convert(i:int):
-        '''
-        Convertie de entier a string
-        '''
-        if(i==0): return "-"
-        if(i==1): return "."
-        if(i==2): return "#"
-    def write_router_output(file):
-        '''
-        Ecrit la solution du placement des routeurs
-        '''
-        f = open(file,"a+")
-        for i in range(rowsN):
-            for j in range(columsN):
-                if(int(pulp.value(mapping_router[(i,j)]))==0):
-                    f.write(convert(mapping[i][j]))
-                else: f.write("r")
-            f.write("\n")
-        f.close()
-    def write_target_output(file):
-        '''
-        Ecrit les cellules qui capte (représenté par un 1 )
-        '''
-        f = open(file,"a+")
-        for i in range(rowsN):
-            for j in range(columsN):
-                if(int(pulp.value(targetcell[(i,j)]))==1):
-                     f.write("1")
-                else: f.write(convert(mapping[i][j]))
-            f.write("\n")
-        f.close()
 
-    # Target cell recoit ou non le signal
-    # Pour chaque mapping_router dans x-r,y-r
+    # target cell recoit ou non le signal
+    # pour chaque mapping_router dans x-r,y-r
     # si (mapping_router==1 et pas de mur entre la cellule et le routeur alors entre alors la cellule = 1)
-    # Pour vérifier si il y a un mur entre, on parcourt la diagonale (n incrémente +1 puis +1 et si on arrive a max on incrémente le deuxieme qui n'a pas finit)
+    # pour vérifier si il y a un mur entre, on parcourt la diagonale (n incrémente +1 puis +1 et si on arrive a max on incrémente le deuxieme qui n'a pas finit)
     for i in range(rowsN):
         for j in range(columsN):
             # Les cellules sur les cotées n'ont pas forcément autour de cellues autour que celles
@@ -140,8 +115,7 @@ def process_router(mapping,router_budget,maxSeconds=None):
             for xr in range(-radius,radius+1):
                 for yr in range(-radius,radius+1):
 
-                    if(i+xr<0 or i+xr>=rowsN or j+yr<0 or j+yr>=columsN):
-                        prob+=pulp.lpSum(targetcell_receive[(i,j,z)]) ==0
+                    if(i+xr<0 or i+xr>=rowsN or j+yr<0 or j+yr>=columsN): prob+=pulp.lpSum(targetcell_receive[(i,j,z)]) ==0
                         # On vérifier qu'il n'y a pas de murs
                     elif(check_wall(i,j,i+xr,j+yr)==0):
                         #-x+y >=0
@@ -151,7 +125,6 @@ def process_router(mapping,router_budget,maxSeconds=None):
                     # Si il y a un mur alors la cellules ne recoit pas
                     else:
                         prob+=pulp.lpSum(targetcell_receive[(i,j,z)]) ==0
-                    
                     z+=1
             # Si dans une seule sur les radius*radius possibilés on capte, alors on capte
             # <=> si la somme des receveurs >=1 then targetcell = 1 else targetcell = 0
@@ -162,34 +135,39 @@ def process_router(mapping,router_budget,maxSeconds=None):
             prob+=pulp.lpSum(0-M*(omega[(i,j)])) <= targetcell[(i,j)]
             prob+=pulp.lpSum(0+M*(omega[(i,j)])) >= targetcell[(i,j)]
 
-
     prob.solve(pulp.PULP_CBC_CMD(maxSeconds=maxSeconds))
     # Compute Score 
     score = 0
     for i in range(rowsN):
         for j in range(columsN):
-            if(int(pulp.value(targetcell[(i,j)]))==1):
-                score+=1
+            if(int(pulp.value(mapping_router[(i,j)]))==1): mapping[i][j] = 3
+            if(int(pulp.value(targetcell[(i,j)]))==1): score+=1
+    return (mapping,score)
+
+
+
+sectionX = 20;sectionY = 20
+crossingX = int(columsN/sectionX);crossingY = int(rowsN/sectionY)
+budgetPerStep = 400
+x=0;y=0;SCORE = 0
+mapping = np.asarray(mapping)
+
+print("\n----Starting resolution ----\n")
+while (x<crossingX):
+    while(y<crossingY):
+        mapp =  mapping[x*sectionX:min((x+1)*sectionX,rowsN),y*sectionY:min((y+1)*sectionY,columsN)]
+        (mapp,score) = process_router(mapp,budgetPerStep,10)
+        mapping[x*sectionX:min((x+1)*sectionX,rowsN),y*sectionY:min((y+1)*sectionY,columsN)] = mapp
+        SCORE+=score;y+=1
+        sys.stdout.write(" %s/%s\r"%(y*sectionY,columsN));sys.stdout.flush()
+
+    x+=1;y=0;print("\nstep %d terminated,  score: %d" % (x, SCORE))
     # Write output
-    write_router_output(CHARLESTON_ROUTER_OUT)
-    write_target_output(CHARLESTON_TARGET_OUT)
+    write_output(CHARLESTON_ROUTER_OUT,mapping[:x*sectionX])
 
 
-    return (prob,score)
 
-# Remet les fichiers output à 0
-reset_output_file(CHARLESTON_ROUTER_OUT)
-reset_output_file(CHARLESTON_TARGET_OUT)
-section = 5
-crossing = int(rowsN/section)
-budgetPerStep = int(router_budget/crossing)
-print("----Starting resolution ----")
-for x in range(0,crossing):
-    (prob,score) = process_router(mapping[x*section:x*section+section],budgetPerStep,10)
-    print("step %d terminated,  score: %d" % (x, score))
-
-
-print("End")
+print("end")
 
 # Adapter router_buget/crossing
 # Calculer meilleur rapport entre budget et nombre de répeteur
