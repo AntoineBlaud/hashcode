ecf638405522332b76a9f0fb0e04f9431cbef652
darkloner99 <antoine.blaud@gmail.com>
2020-02-13 16:40:57 +0100

    pizza regina

---
+++ pizza regina/main.py
@@ -0,0 +1,118 @@
+from guppy import hpy
+import sys
+import os
+import time
+import argparse
+import re
+from copy import copy
+import pulp 
+
+
+# Profilers
+######################################################################################
+h = hpy()
+h.heap()
+######################################################################################
+
+M = 100000
+
+default_in = "C:\\Users\\antoi\\Documents\\GitHub\\hashcode\\pizza regina\\in\\example.in"
+default_out= "out/exemple.out"
+
+
+
+
+
+class Pizza:
+    def __init__(self,mapping,items,i,j):
+        self.mapping = mapping
+        self.items = items
+        self.rowsN = i
+        self.columsN = j
+
+    # coupe la pizza en plusieurs "grosse part", la taille des parts est donné par row_lenght et column_lenght
+    def split_pizza_into_map(self,row_lenght,column_lenght):
+        splited_mapping = []; i=0; j=0;cursorX=0;cursorY=0
+        while(i<rows_N and j<colums_N):
+            new_mapping = []
+            for x in range(i,min(i+column_lenght,self.rowsN)):
+                for y in range(j,min(j+row_lenght,self.columsN)):
+                    new_mapping.append(self.mapping[x*self.columsN+y])
+                    cursorX = x
+                    cursorY = y
+            j+=row_lenght;
+            if(cursorY == self.columsN-1):
+                j = 0
+                i+= column_lenght
+            splited_mapping.append(new_mapping)
+        return splited_mapping
+
+
+
+
+'''
+Cette fonction s'occupe de retourner toutes les découpe possible de part pour un mapping
+il y a donc la même cellule dans plusieurs combinaisons de part 
+Les combinaisons de part se base sur le critère de taille maximum et la quantité minimal d'ingrédient
+'''
+def find_all_combination(mapping):
+
+    #ici juste liste les combinaisons en prenant en compte que la taille maximal
+    combinations_step1 = []
+    combinations_step2 = []
+    y0,x0,item = mapping[0]
+    y1,x1,item = mapping[len(mapping)-1]
+    # pour chaque cellules dans la pizza
+    for i0 in range(x0,x1+1):
+        for j0 in range(y0,y1+1):
+            # teste les combinaisons et ajoute les combinaisons valides
+            for i1 in range(i0,min(x1+1,i0+max_cell)):
+                for j1 in range(j0,min(y1+1,j0+int(max_cell/(i1-i0+1)))):
+                    combinations_step1.append((i0,i1,j0,j1))
+
+    # maintenand on filtre en prenant aussi en compte le nombre minimal d'ingrédient
+    for combination in combinations_step1:
+        x0,x1,y0,y1 = combination
+        tomato = 0
+        mushroom = 0
+        for x in range(x0,x1+1):
+            for y in range(y0,y1+1):
+                item = pizza.items[y][x]
+                if(item=="T"): tomato+=1
+                if(item=="M"): mushroom+=1
+        if(tomato>=min_ing & mushroom>=min_ing):
+            combinations_step2.append(combination)
+    return combinations_step2
+
+
+find_all_combination(pizza.mapping)
+
+def solved_pizza(pizza):
+
+    prob = pulp.LpProblem("Pizza hashcode", pulp.LpMaximize)
+
+
+
+
+
+parser = argparse.ArgumentParser(description='Compute hashcode pizza regina')
+parser.add_argument('-i', '--filein', help='Input file', default=default_in, type=str)
+parser.add_argument('-o', '--fileout', help='Ouput file', default=default_out, type=str)
+parser.add_argument('-v', '--verbose', default=-1,
+                    help='verbose mode', type=int)
+args = parser.parse_args()
+
+mapping = []
+items = []
+with open(args.filein) as f:
+    rows_N,colums_N,min_ing,max_cell = map(int,[int(x) for x in re.sub("\n","",f.readline()).split(" ")])
+    line = [list(re.sub("\n","",f.readline()))for i in range(rows_N)]
+    mapping = ([(i,j,line[i][j]) for i in range(rows_N) for j in range(colums_N)])
+    items = ([[line[i][j] for j in range(colums_N) ]for i in range(rows_N)])
+
+pizza = Pizza(mapping,items,rows_N,colums_N)
+#test
+pizzas = pizza.split_pizza_into_map(2,3)
+
+
+
