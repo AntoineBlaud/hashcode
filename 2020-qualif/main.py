69d60bbb4f3d9e2db067d922b69c203611a841ab
darkloner99 <antoine.blaud@gmail.com>
2020-02-20 19:19:57 +0100

    input done

--- 2020-qualif/main.py
+++ 2020-qualif/main.py
@@ -34,16 +34,13 @@ import tqdm
 #########################################################
 if __name__ == '__main__':
 
-    DEFAULT_F = ""
+    DEFAULT_F = "C:\\Users\\antoi\\Documents\\GitHub\\hashcode\\2020-qualif\\in\\a_example.txt"
     OUTPUT_F = ""
 
     parser = argparse.ArgumentParser(description='Hashcode 2020 qualif')
-    parser.add_argument('-i', '--filein', help='Input file',
-                        default=DEFAULT_F, type=str)
-    parser.add_argument('-o', '--fileout', help='Ouput file',
-                        default=OUTPUT_F, type=str)
-    parser.add_argument('-v', '--verbose', default=-1,
-                        help='verbose mode', type=int)
+    parser.add_argument('-i', '--filein', help='Input file',default=DEFAULT_F, type=str)
+    parser.add_argument('-o', '--fileout', help='Ouput file',default=OUTPUT_F, type=str)
+    parser.add_argument('-v', '--verbose', default=-1,help='verbose mode', type=int)
     args = parser.parse_args()
 
     # map(int, [int(x) for x in re.sub("\n", "", f.readline()).split(" ")])
@@ -52,8 +49,18 @@ if __name__ == '__main__':
 
     # Input process
     with open(args.filein) as f:
-        pass
+        booksN,librarieN,deadline = map(int, [int(x) for x in re.sub("\n", "", f.readline()).split(" ")])
+        booksValue  = [int(x) for x in re.sub("\n", "", f.readline()).split(" ")]
+        booksValue = [(i,booksValue[i]) for i in range(len(booksValue))]
 
+        librairies = []
+        for i in range(librarieN):
+            (i,librairiesBooksN,signupProcess) = map(int, [int(x) for x in re.sub("\n", "", f.readline()).split(" ")])
+            books = [int(x) for x in re.sub("\n", "", f.readline()).split(" ")]
+            books = set(books)
+            librairies.append((i,librairiesBooksN,signupProcess,books))
+
+    print("OK")
     # output process
     with open(args.fileout) as f:
         pass
