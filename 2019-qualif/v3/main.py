75cbc0bb901415a9f84cab84b4ccb79aba899b90
darkloner99 <antoine.blaud@gmail.com>
2020-02-20 08:56:42 +0100

    2019 v3 finished

--- 2019-qualif/v3/main.py
+++ 2019-qualif/v3/main.py
@@ -8,17 +8,15 @@ import pulp
 from functools import reduce
 import pickle
 import multiprocessing
-import numpy as np
 import random
-import matplotlib.pyplot as plt
 import datetime
 import tqdm
 
 
 if __name__ == '__main__':
 
-    DEFAULT_F = "C:\\Users\\antoi\\Documents\\GitHub\\hashcode\\2019-qualif\\qualification_round_2019.in\\a_example.txt"
-    OUTPUT_F = "C:\\Users\\antoi\\Documents\\GitHub\\hashcode\\2019-qualif\\out\\a_example.txt"
+    DEFAULT_F = "C:\\Users\\antoi\\Documents\\GitHub\\hashcode\\2019-qualif\\qualification_round_2019.in\\b_lovely_landscapes.txt"
+    OUTPUT_F = "C:\\Users\\antoi\\Documents\\GitHub\\hashcode\\2019-qualif\\out\\b_lovely_landscapes.txt"
 
     parser = argparse.ArgumentParser(
         description='Compute hashcode pizza regina')
@@ -43,32 +41,14 @@ def calculate_common_tags(p1, p2):
         if(tag in p2): score += 1
     return score
 
-def get_tags_in_common(p1,p2):
-    tags = []
-    for tag in p1[2:]:
-        if(tag in p2[2:]): tags.append(tag)
-
-
-def calcule_tags_in_1_but_not_in_2(p1, p2):
-    score = 0
-    for tag in p1:
-        if(tag not in p2): score += 1
-    return score
-
-
-def calcule_tags_in_2_but_not_in_1(p1, p2):
-    score = 0
-    for tag in p2:
-        if(tag not in p1):score+=1
-    return score
 
 # H1
 def get_tags_in_H_slide(photos,index):
-    return photos[index][2:]
+    return photos[index][3]
 
 # (V1,V2)
 def get_tags_in_V_slide(photos,indexs):
-    return get_tags_in_common(photos[indexs[0]],photos[indexs[1]])
+    return photos[indexs[0]][3].union(photos[indexs[1]][3])
 
 def get_tags(photos,index):
     if(type(index) is int): #--> H slide
@@ -76,10 +56,12 @@ def get_tags(photos,index):
 
     return get_tags_in_V_slide(photos,index)
 
-def calculate_score(tags1,tags2):
-    score1 = calcule_tags_in_1_but_not_in_2(tags1,tags2)
-    score2 = calcule_tags_in_2_but_not_in_1(tags1,tags2)
-    score3 = calculate_common_tags(tags1,tags2)
+def calculate_score(tags1:set,tags2:set):
+    p = tags1.intersection(tags2)
+    score1 = len(p)
+    score2 = len(tags1) - len(p)
+    score3 = len(tags2) -len(p)
+    return min(score1,score2,score3)
 
 
 
@@ -93,12 +75,14 @@ for photo in photos:
 
 # on groupe par 2 les photos verticales qui ont le moins de tags en commun
 photosVBy2 = []
+pbar = tqdm.tqdm(total = len(photosV))
 while(len(photosV) > 0):
     photo1 = photosV[0]
     bestChoice = 1
-    bestScore = calculate_common_tags(photo1[2:],photosV[bestChoice][2:])
-    for i,photo2 in enumerate(photosV[1:]):
-        common = calculate_common_tags(photo1[2:],photo2[2:])
+    bestScore = calculate_common_tags(photo1[3:],photosV[bestChoice][3:])
+    for i in range(1,min(4000,len(photosV))):
+        photo2 = photosV[i]
+        common = calculate_common_tags(photo1[3:],photo2[3:])
         if(common<bestScore):
             bestScore = common
             bestChoice = i
@@ -106,17 +90,60 @@ while(len(photosV) > 0):
     photosVBy2.append(((photo1[0],photosV[bestChoice][0])))
     del photosV[0]
     del photosV[bestChoice-1]
+    pbar.update(2)
 
 photosIndexs = photosH + photosVBy2
+for photo in photos:
+    photo[3] = set(photo[3:])
+    del photo[4:]
 
-pbar = tqdm.tqdm(total = len(photosIndexs))
-while(len(photosIndexs)>0):
-    photo1 = photosIndexs[0]
-    photo1Tags = get_tags(photos,photo1)
-    points = 0
-    # entrée pair donc pas besoin de vérifier la taille
-    for photo2 in photosIndexs[1:]:
-        photo2Tags = get_tags(photos,photo2)
+def solve(photosIndexs):
+    slide = []
+    pbar = tqdm.tqdm(total = len(photosIndexs))
+    FinalScore = 0
+    random.shuffle(photosIndexs)
+    while(len(photosIndexs)>1):
+        photo1 = photosIndexs[0]
+        photo1Tags = get_tags(photos,photo1)
+        points = 0
+        best = 1
+        # entrée pair donc pas besoin de vérifier la taille
+        for i in range(1,min(20000,len(photosIndexs))):
+            photo2 = photosIndexs[i]
+            photo2Tags = get_tags(photos,photo2)
+            s = calculate_score(photo1Tags,photo2Tags)
+            if(s > points):
+                points = s
+                best = i
+
+
+    
+        slide.append(photo1)
+        slide.append(photosIndexs[best])
+        del photosIndexs[0]
+        del photosIndexs[best-1]
+        pbar.update(2)
+        FinalScore+=points
+        pbar.set_postfix(score=FinalScore)
+
+    if(len(photosIndexs)>0):
+        slide.append(photosIndexs[0])
+        pbar.update(1)
+    
+
+    with open(args.fileout,"w") as f:
+        f.write(str(len(slide))+"\n")
+        for e in slide:
+            if(type(e)==tuple):
+                f.write(str(e[0])+" "+str(e[1])+"\n")
+            else:
+                f.write(str(e)+"\n")
+
+
+solve(photosIndexs)
+    
+
+
 
 
 
--- test.py
+++
@@ -1,9 +0,0 @@
-import tqdm
-
-b=  []
-for i in tqdm.tqdm(range(10000)):
-    for x in range(10000):
-        for y in range(100):
-            b.append(x)
-    
-    
